6823974e30588a4f54548bcec4aa9b952248b301
Radik Abubjazov <abubjazov@mail.ru>
2021-12-21 21:48:02 +0300

    PostController.js: +deleteById()

--- PostController.js
+++ PostController.js
@@ -56,7 +56,15 @@ class PostController {
 
     async deleteById(req, res) {
         try {
+            const { id } = req.params
 
+            if (!id) {
+                return res.status(400).json({ message: 'No ID specified' })
+            }
+
+            const post = await Post.findByIdAndDelete(id)
+
+            return res.json(post)
         } catch (error) {
             res.status(500).json(error)
         }
