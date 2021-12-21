fc45b6b6d7ea9b8f82bb659ebaf68215e8343511
Radik Abubjazov <abubjazov@mail.ru>
2021-12-21 20:29:45 +0300

    app: +/setup - mongoose

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
-const PORT = process.env.PORT
 const app = express()
 
 app.use(express.json())
 
 app.post('/', (req, res) => {
     console.log(req.body)
-    res.status(200).json('Server at work 123')
+    res.status(200).json('Server at work')
 })
 
-app.listen(PORT, () => {
-    console.log(`Server started on PORT: ${PORT}`)
-})
+async function startApp() {
+    try {
+        await mongoose.connect(process.env.DB_URL)
+        app.listen(process.env.PORT, () => {
+            console.log(`Server started on PORT: ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startApp()
