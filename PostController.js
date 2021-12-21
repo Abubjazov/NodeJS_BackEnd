1cde54bf3485fb338c40e7dc636111e629ee774c
Radik Abubjazov <abubjazov@mail.ru>
2021-12-21 21:30:21 +0300

    PostController.js: +getById()

--- PostController.js
+++ PostController.js
@@ -24,7 +24,15 @@ class PostController {
 
     async getById(req, res) {
         try {
+            const { id } = req.params
 
+            if (!id) {
+                return res.status(400).json({ message: 'No ID specified' })
+            }
+
+            const post = await Post.findById(id)
+
+            return res.json(post)
         } catch (error) {
             res.status(500).json(error)
         }
